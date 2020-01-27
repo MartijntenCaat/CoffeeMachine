4e5e6bf8da316136e0380c4c512cdf55ac04f84b
MartijntenCaat <tencaat@gmail.com>
2020-01-27 13:08:40 +0100

    Feedback J. verwerkt

--- CoffeeMachine.java
+++ CoffeeMachine.java
@@ -10,72 +10,71 @@ public class CoffeeMachine {
     private int currentGramOfBeans;
     private int currentNumberOfCups;
     private int currentChange;
-    private boolean isOnline;
+    private boolean isOnline = true;
 
-    public CoffeeMachine(boolean isOnline) {
+    private CoffeeMachine() {
         this.scanner = new Scanner(System.in);
         this.currentMlOfWater = 400;
         this.currentMlOfMilk = 540;
         this.currentGramOfBeans = 120;
         this.currentNumberOfCups = 9;
         this.currentChange = 550;
-        this.isOnline = true;
     }
 
-    public int getCurrentMlOfWater() {
+    private int getCurrentMlOfWater() {
         return currentMlOfWater;
     }
 
-    public int getCurrentMlOfMilk() {
+    private int getCurrentMlOfMilk() {
         return currentMlOfMilk;
     }
 
-    public int getCurrentGramOfBeans() {
+    private int getCurrentGramOfBeans() {
         return currentGramOfBeans;
     }
 
-    public int getCurrentNumberOfCups() {
+    private int getCurrentNumberOfCups() {
         return currentNumberOfCups;
     }
 
-    public int getCurrentChange() {
+    private int getCurrentChange() {
         return currentChange;
     }
 
-    public boolean getIsOnline() {
+    private boolean IsOnline() {
         return isOnline;
     }
 
-    public void setCurrentMlOfWater(int water) {
+    private void addWater(int water) {
         currentMlOfWater += water;
     }
 
-    public void setCurrentMlOfMilk(int milk) {
+    private void addMilk(int milk) {
         currentMlOfMilk += milk;
     }
 
-    public void setCurrentGramOfBeans(int beans) {
+    private void addBeans(int beans) {
         currentGramOfBeans += beans;
     }
 
-    public void setCurrentNumberOfCups(int cups) {
+    private void addCups(int cups) {
         currentNumberOfCups += cups;
     }
 
-    public void setCurrentChange(int change) {
+    private void subtractChange(int change) {
         currentChange -= change;
     }
 
-    public void setIsOline(boolean state){
-        isOnline = state;
+    private void setOffline() {
+        isOnline = false;
     }
 
-    public String nextAction() {
+    private String nextAction() {
         System.out.println("Write action (buy, fill, take, remaining, exit):");
         return scanner.nextLine();
     }
 
-    public void brewCoffee(int water, int milk, int beans, int cups, int change) {
+    private void makeCoffee(int water, int milk, int beans, int cups, int change) {
         if (getCurrentMlOfWater() - water < 0) {
             System.out.println("Sorry, not enough water!");
         } else if (getCurrentMlOfMilk() - milk < 0) {
@@ -94,48 +93,46 @@ public class CoffeeMachine {
         }
     }
 
-    public void fill() {
+    private void fillCoffeeMachine() {
         System.out.println("Write how many ml of water do you want to add:");
-        setCurrentMlOfWater(scanner.nextInt());
+        addWater(scanner.nextInt());
 
         System.out.println("Write how many ml of milk do you want to add:");
-        setCurrentMlOfMilk(scanner.nextInt());
+        addMilk(scanner.nextInt());
 
         System.out.println("Write how many grams of coffee beans do you want to add:");
-        setCurrentGramOfBeans(scanner.nextInt());
+        addBeans(scanner.nextInt());
 
         System.out.println("Write how many disposable cups of coffee do you want to add:");
-        setCurrentNumberOfCups(scanner.nextInt());
+        addCups(scanner.nextInt());
     }
 
-    public void buy() {
+    private void buyCoffee() {
 
         System.out.println("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino, back - to main menu: ");
         String buyAction = scanner.nextLine();
 
         switch (buyAction) {
             case "1": // For the espresso, the coffee machine needs 250 ml of water and 16 g of coffee beans. It costs $4.
-                brewCoffee(250, 0, 16, 1, 4);
+                makeCoffee(250, 0, 16, 1, 4);
                 break;
             case "2": // For the latte, the coffee machine needs 350 ml of water, 75 ml of milk, and 20 g of coffee beans. It costs $7.
-                brewCoffee(350, 75, 20, 1, 7);
+                makeCoffee(350, 75, 20, 1, 7);
                 break;
             case "3": // And for the cappuccino, the coffee machine needs 200 ml of water, 100 ml of milk, and 12 g of coffee. It costs $6.
-                brewCoffee(200, 100, 12, 1, 6);
-                break;
-            case "back":
+                makeCoffee(200, 100, 12, 1, 6);
                 break;
             default:
                 break;
         }
     }
 
-    public void take() {
+    private void takeChangeFromMachine() {
         System.out.println("I gave you $" + getCurrentChange());
-        setCurrentChange(getCurrentChange());
+        subtractChange(getCurrentChange());
     }
 
-    public void remaining() {
+    private void printRemainingStock() {
         System.out.println("The coffee machine has:");
         System.out.println(getCurrentMlOfWater() + " of water");
         System.out.println(getCurrentMlOfMilk() + " of milk");
@@ -144,36 +141,36 @@ public class CoffeeMachine {
         System.out.println("$" + getCurrentChange() + " of money");
     }
 
-    public void exit(){
-        setIsOline(false);
+    private void shutdownCoffeeMachine() {
+        setOffline();
         scanner.close();
     }
 
     public static void main(String[] args) {
 
-        CoffeeMachine coffeeMachine = new CoffeeMachine(true);
+        CoffeeMachine coffeeMachine = new CoffeeMachine();
 
-        while (coffeeMachine.getIsOnline()) {
+        while (coffeeMachine.IsOnline()) {
 
             switch (coffeeMachine.nextAction()) {
                 case "buy":
-                    coffeeMachine.buy();
+                    coffeeMachine.buyCoffee();
                     break;
 
                 case "fill":
-                    coffeeMachine.fill();
+                    coffeeMachine.fillCoffeeMachine();
                     break;
 
                 case "take":
-                    coffeeMachine.take();
+                    coffeeMachine.takeChangeFromMachine();
                     break;
 
                 case "remaining":
-                    coffeeMachine.remaining();
+                    coffeeMachine.printRemainingStock();
                     break;
 
                 case "exit":
-                    coffeeMachine.exit();
+                    coffeeMachine.shutdownCoffeeMachine();
                     break;
 
                 default:
